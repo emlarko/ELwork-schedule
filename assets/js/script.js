dfd9ae55cbf0188b2bf0cb2238d0982c65fe8386
Emily Larkin <emilylarkin@Emilys-MacBook-Air.local>
2023-01-23 00:52:54 +0000

    day.js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,73 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+
+
+ $(function () {
+
+  var eventData;
+
+  function renderData(){
+
+    if (localStorage.getItem('eventData') !== null) {
+      eventData=JSON.parse(localStorage.getItem('eventData'));
+      $('.description').each(function (button_id){
+      $(this).val(eventData[button_id]);
+          }); 
+    } else {
+        eventData={};
+        for (let i=1;i<=9;i++){
+        eventData[i]="";
+        }
+        console.log(eventData)
+    }
+  }
+
+  renderData();
+
+  function time() {
+
+  var currentTime = dayjs().format('HH:mm:ss');
+  console.log(currentTime);
+  
+  $('.time-block').each(function (eventTime) {
+    var eventTime = $(this).attr('id');
+
+    if(eventTime === currentTime){
+      $(this).addClass('present');
+    }
+
+    else if(eventTime > currentTime){
+      $(this).addClass('future');
+    }
+
+    else {
+      $(this).addClass('past');
+    }
+
+  })
+  };
+
+  setInterval(time(), 1000);
+
+  
+  $('.btn').click(function(event){
+  event.preventDefault();
+    var text = $(this).prev().val();
+    console.log("text", text);
+    var button_id=$(this).attr('id');
+    eventData[button_id]=text;
+    localStorage.setItem('eventData', JSON.stringify(eventData));
+  
+})
+
+function date() {
+
+  var today = dayjs();
+  $('#currentDay').text(today.format('dddd, MMMM Do'));
+}
+
+setInterval(date(), 1000);
+ 
+})
+
